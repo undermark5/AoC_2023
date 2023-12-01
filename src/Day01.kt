75e92a6ff1d17ff410c48fdb81bda6fa8658cbcb
Undermark5 <undermark5@undermark5.com>
2023-12-01 12:45:58 -0600

    Day 01 both stars

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,33 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map {
+            it.filter { it.isDigit() }
+        }.sumOf {
+            (it.first().toNumericValue()) * 10 + (it.last().toNumericValue())
+        }
     }
 
+
+    val digits = (1..9).map(Int::toString) + (englishDigits - "zero").keys
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.mapNotNull {
+            val first = it.findAnyOf(
+                digits
+            )?.let {(_, value) ->
+                value.parseDigit()
+            }
+            val last = it.findLastAnyOf(
+                digits
+            )?.let {(_, value) ->
+                value.parseDigit()
+            }
+            first?.times(10)?.plus(last ?: 0)
+        }.sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readExampleInput("Day01")
+    check(part1(testInput).also(Any::println) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,12 +1,20 @@
+import kotlinx.coroutines.*
 import java.math.BigInteger
 import java.security.MessageDigest
+import java.util.concurrent.ConcurrentHashMap
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
+
 
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = Path("src/$name.txt").readLines()
+fun readInput(name: String) = Path("inputs/$name.txt").readLines()
+
+/**
+ * Reads lines from the given input txt file.
+ */
+fun readExampleInput(name: String) = Path("inputs/sample/$name.txt").readLines()
 
 /**
  * Converts string to md5 hash.
@@ -19,3 +27,132 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+fun <E> MutableList<E>.push(c: E) {
+    this.add(0, c)
+}
+
+fun <E> MutableList<E>.pop(): E {
+    return this.removeAt(0)
+}
+
+fun <E> MutableList<E>.peek(): E {
+    return this.first()
+}
+
+data class MutablePair<T, O>(var first: T, var second: O)
+
+fun String.toIntRange(delim: String = ".."): IntRange {
+    val split = this.split(delim)
+    return split.first().toInt()..split.last().toInt()
+}
+
+operator fun <R> (() -> R).times(paddingSize: Int): List<R> {
+    val value = this()
+    return MutableList(paddingSize) { value }
+}
+
+operator fun IntRange.contains(intRange: IntRange): Boolean {
+    return first <= intRange.first && intRange.last <= last
+}
+
+operator fun <E> List<MutableList<E>>.set(pair: Pair<Int, Int>, value: E) {
+    this[pair.first][pair.second] = value
+}
+
+operator fun <K, V> MutableMap<K, MutableMap<K, V>>.get(pair: Pair<K, K>): V? {
+    return this[pair.first]?.get(pair.second)
+}
+
+operator fun <K, V> MutableMap<K, MutableMap<K, V>>.set(pair: Pair<K, K>, value: V) {
+    this.getOrPut(pair.first) {
+        ConcurrentHashMap()
+    }[pair.second] = value
+}
+
+operator fun <K, V> ConcurrentHashMap<K, ConcurrentHashMap<K, V>>.get(pair: Pair<K, K>): V? {
+    return this[pair.first]?.get(pair.second)
+}
+
+operator fun <K, V> ConcurrentHashMap<K, ConcurrentHashMap<K, V>>.set(pair: Pair<K, K>, value: V) {
+    this.getOrPut(pair.first) {
+        ConcurrentHashMap()
+    }[pair.second] = value
+}
+
+operator fun <E> List<List<E>>.get(pair: Pair<Int, Int>): E {
+    return this[pair.first][pair.second]
+}
+
+fun <E> List<List<E>>.getOrNull(pair: Pair<Int, Int>): E? {
+    return this.getOrNull(pair.first)?.getOrNull(pair.second)
+}
+
+open class Node<E>(val value: E) {
+    var visited = false
+    var neighbors = listOf<Node<E>>()
+}
+
+
+fun <E> List<E>.toPair(): Pair<E, E> {
+    return first() to last()
+}
+
+val <A, B> Pair<A, B>.reversed: Pair<B, A>
+    get() = second to first
+
+suspend fun <K, V> MutableMap<K, V>.parallelForEach(block: suspend (Map.Entry<K, V>) -> Unit) {
+
+    val jobsToScopes = this.map {
+        val scope = CoroutineScope(Dispatchers.IO)
+        scope.launch { block(it) } to scope
+    }
+    val jobs: List<Job> = jobsToScopes.map { it.first }
+    val scopes = jobsToScopes.map { it.second }
+    jobs.joinAll()
+    scopes.forEach(CoroutineScope::cancel)
+}
+
+suspend fun <K, V, R> Map<K, V>.parallelMap(block: suspend (Map.Entry<K, V>) -> R): List<R> {
+    println("starting ${this.size} jobs")
+    val totalJobs = this.size
+    var jobId = 1
+    val jobsToScopes = this.map {
+        val scope = CoroutineScope(Dispatchers.IO)
+        scope.async {
+            val myId = jobId++
+            block(it).also { println(myId) }
+        } to scope
+
+    }
+    val jobs = jobsToScopes.map { it.first }
+    val results = jobs.awaitAll()
+    println("jobs complete")
+    jobsToScopes.forEach { it.second.cancel() }
+    return results
+}
+
+fun Char.toNumericValue(): Int {
+    if (!isDigit()) error("can't convert non-digits")
+    return minus('0')
+}
+
+val englishDigits = mapOf(
+    "zero" to 0,
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9,
+)
+
+fun String.parseDigit(): Int? {
+    return if (length == 1) {
+        toIntOrNull()
+    } else {
+        englishDigits[this]
+    }
+}
